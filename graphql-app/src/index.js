0f805aa856ed90bca4909e05f0fbb2be835c742a
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-04-06 14:44:12 +0700

    ⚡️ query

--- graphql-app/src/index.js
+++ graphql-app/src/index.js
@@ -1,37 +1,67 @@
-const { GraphQLServer } = require('graphql-yoga');
+// const { GraphQLServer } = require('graphql-yoga');
+// ! err -> ⬇ use this below ⬇
+const { createServer } = require('graphql-yoga');
 
-const typeDefs = `
-    type Query {
-        name: String!
-        age: Int!
-        isSingle: Boolean
-    }
-`
-
-const resolvers = {
-    Query: {
-        name() {
-            return 'asd';
-        },
-        age() {
-            return 22;
-        },
-        isSingle() {
-            return null
+const users = [
+    {
+        id: '1',
+        name: 'john',
+        age: '25',
+        location: {
+            state: 'Bangkok',
+            city: '111'
+        }
+    },
+    {
+        id: '2',
+        name: '2',
+        age: '2',
+        location: {
+            state: 'Bangkok2',
+            city: '2'
         }
     }
-}
+]
 
-const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
+// Create your server
+const server = createServer({
+    schema: {
+        typeDefs: /* GraphQL */ `
+            type Query {
+                name: String!
+                age: Int!
+                isSingle: Boolean
+                number: [Int!]!
+                location: Location
 
-const options = {
-    port: 4000,
-    endpoint: '/graphql'
-}
-
-server.start(options, ({ port }) => 
-    console.log(`server on port ${port}.`)
-)
+                users: [User!]!
+            }
+            type User {
+                id: ID!
+                name: String!
+                age: Int!
+                location: Location!
+            }
+            type Location {
+                state: String!
+                city: String!
+            }
+        `,
+        // o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o
+        resolvers: {
+            Query: {
+                name: () => 'sisi',
+                age: () => 22,
+                isSingle: () => null,
+                number: () => [10, 20, 30, 40 ,50],
+                location: () => ({
+                    state: () => 'Bangkok',
+                    city: () => 'Ladkrabung'
+                }),
+                users: () => users
+            },
+        },
+    },
+  })
+  // start the server and explore http://localhost:4000/graphql
+  server.start()
