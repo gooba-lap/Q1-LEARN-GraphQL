a5507311bea0de65545e723edc67097237dcdc09
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-04-06 10:27:46 +0700

    ⚡️ mutation

--- current-project/server.js
+++ current-project/server.js
@@ -28,6 +28,21 @@ const books = [
 	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
 ]
 
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'This represents a author of a book',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter(book => book.authorId === author.id)
+            }
+        }
+    }) 
+})
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     description: 'This represents a book written by an author',
@@ -44,29 +59,74 @@ const BookType = new GraphQLObjectType({
     }) 
 })
 
-const AuthorType = new GraphQLObjectType({
-    name: 'Author',
-    description: 'This represents a author of a book',
-    fields: () => ({
-        id: { type: new GraphQLNonNull(GraphQLInt)},
-        name: { type: GraphQLNonNull(GraphQLString) },
-    }) 
-})
-
 const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A Single Book',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => books.find(book => book.id === args.id)
+        },
         books: {
             type: new GraphQLList(BookType),
             description: 'List of all Books',
             resolve: () => books
+        },
+        authors: { 
+            type: new GraphQLList(AuthorType),
+            description: 'List of ALL Authors',
+            resolve: () => authors
+        },
+        author: {
+            type: AuthorType,
+            description: 'A Single Author',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => authors.find(author => author.id === args.id)
         }
     })
 })    
 
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a book',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                authorId: { type: GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
+                books.push(book)
+                return book
+            }
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an author',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parent, args) => {
+                const author = { id: author.length + 1, name: args.name }
+                author.push(author)
+                return author
+            }
+        }
+    })
+})
+
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 })
 
 // * query { message }
--- document.md
+++ document.md
@@ -1,6 +1,6 @@
 ## Set env
 
-```bash
+```
 npm init
 
 npm i express express-graphql graphql
@@ -8,13 +8,35 @@ npm i express express-graphql graphql
 npm i --save-dev nodemon
 ```
 
+<!-- o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o -->
+
 ## run
 
 ```bash
 npm run devStart
 ```
 
+<!-- o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o -->
+
 ## Config bug 
 
 expressGraphQL is not a function
 - https://stackoverflow.com/questions/65517979/expressgraphql-is-not-a-function
+
+<!-- o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o o-o -->
+
+## GraphiQL
+
+### mutation
+```
+mutation {
+  addBook(name: "new name", authorId: 1) {
+    id
+    name
+  }
+}
+```
+
+### query
+
+###
