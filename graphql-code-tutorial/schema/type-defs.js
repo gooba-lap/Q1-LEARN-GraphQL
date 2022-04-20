94688c9aeaa5dc045aba864e18bc0daf110682d2
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-04-20 17:01:17 +0700

    GraphQL Resolvers

--- graphql-code-tutorial/schema/type-defs.js
+++ graphql-code-tutorial/schema/type-defs.js
@@ -7,10 +7,30 @@ const typeDefs = gql`
         username: String!
         age: Int!
         nationality: String!
+        friends: [User]
+        favoriteMovies: [Movie]
+    }
+
+    type Movie {
+        id: ID!
+        name: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!
     }
 
     type Query {
         users: [User!]!
+        user(id: ID!): User!
+        movies: [Movie!]!
+        movie(name: String!): Movie!
+    }
+
+    enum Nationality {
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
     }
 `;
 
