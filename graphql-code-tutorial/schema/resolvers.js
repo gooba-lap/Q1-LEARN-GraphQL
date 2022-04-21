f5c0585c80f6679de5e371b539406b556ea4bb4c
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-04-21 16:20:30 +0700

    Mutations

--- graphql-code-tutorial/schema/resolvers.js
+++ graphql-code-tutorial/schema/resolvers.js
@@ -24,12 +24,42 @@ const resolvers = {
         },
     },
     User: {
-        favoriteMovies: () => {
+        favoriteMovies: () => { 
             return _.filter(
                 MovieList,
                 (movie) => 
                     movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
             )
+        }
+    },
+
+    Mutation: {
+        createUser: (parent, args) => {
+            const user = args.input;
+            // console.log(user);
+            const lastId = UserList[UserList.length - 1].id; 
+            user.id = lastId + 1;
+            UserList.push(user);
+            return user;
+        },
+
+        updateUsername: (parent, args) => {
+            const { id, newUsername } = args.input;
+            let userUpdated;
+            UserList.forEach((user) => {
+                if (user.id === Number(id)) {
+                    user.username = newUsername;
+                    userUpdated = user;
+                }
+            });
+
+            return userUpdated;
+        },
+
+        deleteUser: (parent, args) => {
+            const id = args.id;
+            _.remove(UserList, (user) => user.id === Number(id));
+            return null; 
         }
     }
 }
--- graphql-code-tutorial/schema/type-defs.js
+++ graphql-code-tutorial/schema/type-defs.js
@@ -25,6 +25,25 @@ const typeDefs = gql`
         movie(name: String!): Movie!
     }
 
+    input CreateUserInput {
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality = BRAZIL
+    }
+
+    input UpdateUsernameInput {
+        id: ID!
+        newUsername: String!
+    }
+
+
+    type Mutation {
+        createUser(input: CreateUserInput!): User
+        updateUsername(input: UpdateUsernameInput!): User
+        deleteUser(id: ID!): User
+    }
+
     enum Nationality {
         CANADA
         BRAZIL
@@ -33,5 +52,5 @@ const typeDefs = gql`
         CHILE
     }
 `;
-
+ 
 module.exports = { typeDefs };
